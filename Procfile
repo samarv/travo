4cdc0508a6053d21c035cb1abb23eb13e6e716d5
samarvir <samarvir1996@gmail.com>
2019-09-22 14:28:24 +0530

    add hello world view

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
+release: python manage.py migrate
 web: gunicorn travo.wsgi --log-file -
--- travo/settings.py
+++ travo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@&yrwo32(&wh#q)hddusag%+op%x49*%&vel+tt+r72&roum$$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["travoslack.herokuapp.com"]
+ALLOWED_HOSTS = ["travoslack.herokuapp.com", "127.0.0.1", "localhost"]
 
 
 # Application definition
