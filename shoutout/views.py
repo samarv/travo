d4f9de49ed2dc5771202ad261fd67030850422ae
samarvir <samarvir1996@gmail.com>
2019-09-22 20:16:39 +0530

    Add DialogBox to response

--- shoutout/views.py
+++ shoutout/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
+import json
 import os
 import slack
 import asyncio
+from rest_framework.parsers import FormParser
 
 client = slack.WebClient(token=os.environ['SLACK_API_TOKEN'])
 
@@ -15,12 +17,47 @@ def send_async_message(channel='#bots', text='Hello world from async'):
     response = client.chat_postMessage(
         channel=channel,
         text=text
+
+    )
+    assert response["ok"]
+    assert response["message"]["text"] == text
+
+
+def send_dialog_open(dialog, trigger_id, channel='#bots', text='Hello world from async', ):
+    response = client.dialog_open(
+        dialog=dialog,
+        trigger_id=trigger_id,
+        channel=channel,
+        text=text
+
     )
     assert response["ok"]
     assert response["message"]["text"] == text
 
 
 def command(request):
-    print("boo")
-    send_async_message()
+    data = FormParser().parse(request)
+    print(data)
+    trigger_id = data['trigger_id']
+    dialog = json.dumps({
+        "title": 'Send some praise',
+        "callback_id": 'submit-ticket',
+        "submit_label": 'Submit',
+        "elements": [
+            {
+                "label": 'Text',
+                "type": 'text',
+                "name": 'title',
+                "value": "text",
+                "hint": '30 second summary of the problem',
+            },
+            {
+                "label": "Praise To",
+                "name": "praise",
+                "type": "select",
+                "data_source": "users"
+            }
+        ],
+    })
+    send_dialog_open(trigger_id=trigger_id, dialog=dialog)
     return HttpResponse(status=200)
